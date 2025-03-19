f4e313961ba7942bfcb0b465403e8e3be18e720c
WebsiteMaker7493 <nplakhouiri@gmail.com>
2025-03-19 09:33:59 -0700

    Add files via upload

---
+++ beginner-html-site-styled-gh-pages/scripts/main.js
@@ -0,0 +1,6 @@
+const myHeading = document.querySelector("h1");
+myHeading.textContent = "Hello world!";
+
+document.querySelector("img").addEventListener("click",function(){
+	alert("i know where your family lives dont ever click me again you hear me");
+});
