41ecb2e40be7283e15fe08f718b52c5b1df2d26c
WebsiteMaker7493 <nplakhouiri@gmail.com>
2025-03-21 08:35:56 -0700

    Update main.js

--- beginner-html-site-styled-gh-pages/scripts/main.js
+++ beginner-html-site-styled-gh-pages/scripts/main.js
@@ -1,6 +1,31 @@
 const myHeading = document.querySelector("h1");
 myHeading.textContent = "Hello world!";
 
-document.querySelector("img").addEventListener("click",function(){
-	alert("i know where your family lives dont ever click me again you hear me");
-});
+const myImage = document.querySelector("img");
+myImage.onclick = () => {
+	const mySrc = myImage.getAttribute("src");
+	if (mySrc === "images/firefox-icon.png") {
+		myImage.setAttribute("src","images/firefox2.png");
+	} else {
+		myImage.setAttribute("src","images/firefox-icon.png");
+	}
+};
+
+let myButton = document.querySelector("button");
+
+function setUserName(){
+	const myName = prompt("Please enter you name");
+	localStorage.setItem("name", myName);
+	myHeading.textContent = `Mozilla is cool, ${myName}`;
+}
+
+if (!localStorage.getItem("name")){
+	setUserName();
+} else {
+	const storedName = localStorage.getItem("name");
+	myHeading.textContent = `Mozilla is cool, ${storedName}`;
+}
+
+myButton.onclick = () => {
+	setUserName();
+};
